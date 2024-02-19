5473f78b482c903ccdc9fb0e427f941880612be3
GistelaS <gistelasinurat@gmail.com>
2024-02-19 09:34:12 +0700

    tugas3

--- main/views.py
+++ main/views.py
@@ -25,4 +25,14 @@ def create_food(request):
     context = {'form': form}
     return render(request, "create_food.html", context)
 def show_xml(request):
-    data = Food.objects.all()    
+    data = Food.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def show_json(request):
+    data = Food.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+def show_xml_by_id(request, id):
+    data = Food.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def show_json_by_id(request, id):
+    data = Food.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
